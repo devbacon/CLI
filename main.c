8615a3ecebb6db81e7612dda33dd483493f14749
Devon Bacon <metalogicoder@users.noreply.github.com>
2018-09-21 16:36:48 -0700

    main.c - included libs and defined constants

--- main.c
+++ main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PROMPT "cli-h$ "
+#define MAX_TOKENS 100
+#define COMMANDLINE_BUFSIZE 1024
 
 int main()
 {
-  printf("Hello world!\n");
+
   return 0;
 }
